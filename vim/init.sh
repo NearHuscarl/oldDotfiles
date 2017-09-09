ba8c0fb7a6d982fece2945e8dc7d15263b80100c
Near Huscarl <near.huscarl@gmail.com>
2017-09-09 10:26:29 +0700

    update

--- vim/init.sh
+++ vim/init.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 
+vimPath="$HOME/.vim"
+dirList="undo swapfiles session plugged"
+
 # Make necessary directories if not exists
-if [ ! -d ~/.vim/undo/ ]; then
-   mkdir -p ~/.vim/undo/
-fi
-
-if [ ! -d ~/.vim/swapfiles/ ]; then
-   mkdir -p ~/.vim/swapfiles/
-fi
-
-if [ ! -d ~/.vim/session/ ]; then
-   mkdir -p ~/.vim/session/
-fi
-
-if [ ! -d ~/.vim/plugged/ ]; then
-   mkdir -p ~/.vim/plugged/
-fi
+for dir in $dirList; do
+   if [ ! -d $vimPath/$dir ]; then
+      mkdir -p $vimPath/$dir
+   fi
+done
 
 # Download plug.vim if not exists
 if [ ! -s ~/.vim/autoload/plug.vim ]; then
